bbb902274e70f9c37eeef2e78e26d06ce2952c21
madhavi6305 <91028525+madhavi6305@users.noreply.github.com>
2021-09-20 10:01:41 +0530

    Add files via upload

---
+++ adder.java
@@ -0,0 +1,12 @@
+package info.methodoverloading;
+
+public class adder {
+	public void add(float a,int b)
+	{
+		System.out.println(a+b);
+	}
+	public void add(int a,float b)
+	{
+		System.out.println(a+b);
+	}
+}
